d97f78ca6e76aeed709de653f07b0c4ca16d53d6
Rahul <rahulkarda2002@gmail.com>
2023-02-11 06:09:58 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,104 @@
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+let uppass = [];
+let inpass = [];
+
+signUpButton.addEventListener('click', () => {
+    container.classList.add('right-panel-active');
+});
+
+
+signInButton.addEventListener('click', () => {
+    container.classList.remove('right-panel-active');
+});
+// adding and removing border
+function upimg(element) {
+    var Image = element.querySelector('img');
+    if (Image) {
+        if (Image.classList.contains('clicked')) {
+            Image.classList.remove('clicked');
+            uppass.splice(uppass.indexOf(element.id), 1);
+            // console.log(element.id);
+            // console.log(uppass);
+        }
+        else {
+            Image.classList.add('clicked');
+            uppass.push(element.id);
+            // console.log(element.id);
+            // console.log(uppass);
+        }
+    }
+}
+
+function inimg(element) {
+    var Image = element.querySelector('img');
+    if (Image) {
+        if (Image.classList.contains('clicked')) {
+            Image.classList.remove('clicked');
+            inpass.splice(inpass.indexOf(element.id), 1);
+            // console.log(element.id);
+            // console.log(inpass);
+        }
+        else {
+            Image.classList.add('clicked');
+            inpass.push(element.id);
+            // console.log(element.id);
+            // console.log(inpass);
+        }
+    }
+}
+// element image recognition
+function signup() {
+    sessionStorage.setItem("upname", document.getElementById('upmail').value);
+    sessionStorage.setItem("uppass", uppass);
+    var myText = "Account Created Succesfully";
+    alert(myText);
+}
+// image pattern authentication
+var v2 = new Boolean(false);
+function signin() {
+    let str = document.getElementById('inmail').value;
+    let array = sessionStorage.getItem("uppass");
+    let check1 = array.localeCompare(inpass.toString());
+    if ((!str.localeCompare(sessionStorage.getItem("upname"))) && !check1) {
+        var myText = "Login is successful";
+        alert(myText);
+        NewTab();
+        
+    }
+    else{
+        var myText = "Login Failed";
+        alert(myText);
+        emailjs.init("red7y59yf11Vsfxj2");
+        sendMail3();
+       
+
+    }
+}
+
+var templateParams = {
+    to_name: 'User',
+    from_name: 'Rahul',
+    message: 'Please try again!'
+  };
+
+ function sendMail3(){
+    emailjs.send('service_ktz39ao', 'template_m1h3m8f', templateParams )
+    .then(function(res){
+        // console.log("Success", res.status);
+        alert("mail sent successfully");
+    })
+}
+function sendMail2(){
+    emailjs.send('service_ktz39ao', 'template_m1h3m8f', templateParams)
+    .then(function(res){
+        // console.log("Success", res.status);
+        alert("mail sent successfully");
+    })
+}
+
+function NewTab() {
+    window.open(
+      "https://rahulkarda.me", "_blank");
+}
